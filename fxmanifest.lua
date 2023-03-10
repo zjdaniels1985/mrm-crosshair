63c0b01da06ab0a2cf4d33fd1cbba2239966e50c
Zach Daniels <zjdaniels1985@live.com>
2023-03-09 20:58:54 -0600

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'MrMRVLS'
+description 'Simple crosshair.'
+version '1.1.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/crosshair.png',
+    'ui/index.html',
+    'ui/script.js',
+    'ui/style.css'
+}
